77c7a88e4f34552d139f1479a4ce4d1925a4a148
27elvar <elvar.roff@gmail.com>
2023-11-16 03:02:50 +0400

    ders

--- task3.dart
+++ task3.dart
@@ -1,8 +1,35 @@
 import 'dart:io' ;
 
 void main () {
+   double x,y;
 
-  
+ stdout.writeln('Birinci ededi daxil et');
+x=double.parse(stdin.readLineSync().toString());
+
+stdout.writeln('Ikinci ededi daxil et');
+y=double.parse(stdin.readLineSync().toString());
+
+stdout.writeln('Hansi emeliyyat yerine yetirilir n\ 1-toplama n\ 2-chixma n\ 3-vurma n\ 4-bolme');
+String emel=stdin.readLineSync().toString();
+
+switch(emel){
+case '1':
+print(x+y);
+
+case '2':
+print ('netice :${x-y}');
+
+case '3':
+print ('netice :${x*y}');
+
+case '4':
+print ('netice :${x/y}');
+
+default:print('duzgun deyil');
+
+}
+
+
 }
 
 
